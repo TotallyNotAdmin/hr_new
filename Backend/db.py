43cd1d9b2e25feca55b0b1f097e85da3ee83f20f
TotallyNotAdmin <iwilldeletethisl8rr@gmail.com>
2026-05-19 06:14:10 +0700

    added search path for database schemas

--- Backend/db.py
+++ Backend/db.py
@@ -2,10 +2,14 @@ import asyncpg
 from fastapi import FastAPI
 from const import DATABASE_URL
 
+async def set_search_path(connection):
+    await connection.execute("SET search_path TO app, hr, public")
 
 async def connect_db(app: FastAPI):
-    app.state.pool = await asyncpg.create_pool(DATABASE_URL)
-
+    app.state.pool = await asyncpg.create_pool(
+        DATABASE_URL,
+        init=set_search_path
+    )
 
 async def close_db(app: FastAPI):
     await app.state.pool.close()
