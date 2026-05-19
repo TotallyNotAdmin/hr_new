5ee0f6f2764496191d3ad92e911f1316a2710020
TotallyNotAdmin <iwilldeletethisl8rr@gmail.com>
2026-05-19 22:38:34 +0700

    syntax fix

--- Backend/main.py
+++ Backend/main.py
@@ -18,7 +18,7 @@ app.include_router(employees.router)
 app.include_router(positions.router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
